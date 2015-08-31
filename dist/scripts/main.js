56d7c17b83cbcbe29041a1c848b934845b0457ea
Thien Nguyen <nguyenphongthien@gmail.com>
2015-08-31 14:12:09 +0700

    Update Header and Fonts

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,9 +1,23 @@
 (function() {
+  var toggle;
+
+  toggle = function(item) {
+    switch (item) {
+      case 'hamburger':
+        if ($('#hamburger').length) {
+          return $('#hamburger').toggleClass('active');
+        }
+    }
+  };
+
   $(function() {
-    return $('.carousel').carousel(function() {
+    $('.carousel').carousel(function() {
       return {
         interval: 2000
       };
+    });
+    return $('.action-hamburger').click(function() {
+      return toggle('hamburger');
     });
   });
 
--- src/scripts/main.coffee
+++ src/scripts/main.coffee
@@ -1,3 +1,11 @@
+toggle = (item) ->
+	switch item
+		when 'hamburger'
+			if $('#hamburger').length then $('#hamburger').toggleClass 'active'
+
 $ ->
 	$('.carousel').carousel ->
 		interval: 2000
+
+	$('.action-hamburger').click ->
+		toggle 'hamburger'
